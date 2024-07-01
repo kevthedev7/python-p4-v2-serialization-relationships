943b42d0dfc2c4debe5e5c2a5f4953d4fe3583cd
Mwenda <kelvinfotech@gmail.com>
2024-07-02 00:55:38 +0300

    Python P4 V2 Serialization Relationships

--- server/seed.py
+++ server/seed.py
@@ -1,51 +1,61 @@
-#!/usr/bin/env python3
+# server/models.py
 
-from random import choice as rc
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-from faker import Faker
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
-from app import app
-from models import db, Zookeeper, Animal, Enclosure
+metadata = MetaData(naming_convention=convention)
 
-fake = Faker()
+db = SQLAlchemy(metadata=metadata)
 
-with app.app_context():
 
-    Animal.query.delete()
-    Zookeeper.query.delete()
-    Enclosure.query.delete()
+class Zookeeper(db.Model, SerializerMixin):
+    __tablename__ = 'zookeepers'
 
-    zookeepers = []
-    for n in range(25):
-        zk = Zookeeper(name=fake.name(), birthday=fake.date_between(
-            start_date='-70y', end_date='-18y'))
-        zookeepers.append(zk)
+    # don't forget that every tuple needs at least one comma!
+    serialize_rules = ('-animals.zookeeper',)
 
-    db.session.add_all(zookeepers)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.Date)
 
-    enclosures = []
-    environments = ['Desert', 'Pond', 'Ocean',
-                    'Field', 'Trees', 'Cave', 'Cage']
+    animals = db.relationship('Animal', back_populates='zookeeper')
 
-    for n in range(25):
-        e = Enclosure(environment=rc(environments),
-                      open_to_visitors=rc([True, False]))
-        enclosures.append(e)
 
-    db.session.add_all(enclosures)
+class Enclosure(db.Model, SerializerMixin):
+    __tablename__ = 'enclosures'
 
-    animals = []
-    species = ['Lion', 'Tiger', 'Bear', 'Hippo', 'Rhino', 'Elephant', 'Ostrich',
-               'Snake', 'Monkey']
+    serialize_rules = ('-animals.enclosure',)
 
-    for n in range(200):
-        name = fake.first_name()
-        while name in [a.name for a in animals]:
-            name = fake.first_name()
-        a = Animal(name=name, species=rc(species))
-        a.zookeeper = rc(zookeepers)
-        a.enclosure = rc(enclosures)
-        animals.append(a)
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
-    db.session.add_all(animals)
-    db.session.commit()
+    animals = db.relationship('Animal', back_populates='enclosure')
+
+
+class Animal(db.Model, SerializerMixin):
+    __tablename__ = 'animals'
+
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    enclosure = db.relationship('Enclosure', back_populates='animals')
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+
+    def __repr__(self):
+        return f'<Animal {self.name}, a {self.species}>'
